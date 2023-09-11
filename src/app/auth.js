97add84dda6d4d3994959a4f445c885da6da09d1
Sakti Abdullah Shidiq <saktitamvan@gmail.com>
2023-09-12 00:20:10 +0700

    whoops

--- src/app/auth.js
+++ src/app/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const getUsernameFromToken = (token) => {
     try {
-        const decodedToken = jwt.verify(token, 'egeqwcyyp036ju027d7faj0919rw6mcp');
+        const decodedToken = jwt.verify(token, 'xxxxxx');
         return decodedToken.username;
     } catch (error) {
         return null;
