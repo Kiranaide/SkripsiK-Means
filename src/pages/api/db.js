2dd699818fbeab112fce028cd14e7986c8cf0113
Sakti Abdullah Shidiq <saktitamvan@gmail.com>
2023-09-12 00:18:51 +0700

    whoops

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'aws.connect.psdb.cloud',
-    database: 'skripsiflash',
-    user: '1wuxw3ty5uv1m0cvuv4b',
-    password: 'pscale_pw_cyr7kKqr41k2ZY1A1mlky40JX0cmlrMtIXufsRIa9if',
+    database: 'xxxxxx',
+    user: 'xxxxxx',
+    password: 'xxxxxx',
     ssl:{},
 });
 
